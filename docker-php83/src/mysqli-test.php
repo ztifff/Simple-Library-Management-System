f271558fa1c3580778ba27b0f37cd522425c57aa
ztifff <153470710+ztifff@users.noreply.github.com>
2025-09-28 20:43:18 +0800

    Delete docker-php83 directory

--- docker-php83/src/mysqli-test.php
+++
@@ -1,7 +0,0 @@
-<?php
-$mysqli = new mysqli("db", "root", "rootpassword", "library_db");
-if ($mysqli->connect_error) {
-    die("Connection failed: " . $mysqli->connect_error);
-}
-echo "MySQLi connection successful!";
-?>
--- docker-php83/src/pdo-test.php
+++
@@ -1,8 +0,0 @@
-<?php
-try {
-    $pdo = new PDO("mysql:host=db;dbname=library_db", "root", "root");
-    echo "PDO connection successful!";
-} catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
-}
-?>
